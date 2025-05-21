2b6347af2c10a8477b8bbe28726dc5aa9037a540
Marlix <82500760+Marlix0@users.noreply.github.com>
2025-05-21 09:33:36 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,57 @@ import email from './assets/envelope-fill.png';
 import inst from './assets/instagram.png';
 import fb from './assets/facebook.png';
 import tiktok from './assets/tiktok.png';
+import plus from './assets/plus-circle.png';
+import user from './assets/user.png';
+import SaunaVector from './assets/Vector.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <header id="App-header">
+        <div id="header-left">
+          <img src={logoBalia} alt="Logo"/>
+        </div>
+        <div id="header-center">
+          <img id='header-tel' src={telephone} alt="telephone" width="24px"/><span id='contact-phone'>+48 000 000 000</span>
+          <img src={email} alt="email" class="icons" width="32px"/><span id='contact-email'>kontakt@firma.pl</span>
+        </div>
+        <div id="header-right">
+          <a href="#"><img src={plus} alt="plus-icon" class="icons" width="20px"/>Dodaj ogłoszenie</a>
+          <button class="login-btn"><img src={user} alt='user' />Zaloguj się</button>
+        </div>
       </header>
       <main>
-
+        <div id='App-content'>
+          <div id='Register-content'>
+            <img id='Sauna-pic' src={SaunaVector} alt='sauna-pic'/>
+            <div id='Register-div'>
+              <p id='Register-title'>Utwórz swoje konto</p>
+              <p id='HaveAccount'>Masz już konto? <a>Zaloguj się</a></p>
+              <div className='TextField-container'>
+                <label htmlFor='Name-input'>Imię</label>
+                <input type='text' id='Name-input' className='TextFields' placeholder='Wpisz imię...'></input>
+              </div>
+              <div className='TextField-container'>
+                <label htmlFor='Surname-input'>Nazwisko</label>
+                <input type='text' id='Surname-input' className='TextFields' placeholder='Wpisz nazwisko...'></input>
+              </div>
+              <div className='TextField-container'>
+                <label htmlFor='Email-input'>Email</label>
+                <input type='email' id='Email-input' className='TextFields' placeholder='email@gmail.com'></input>
+              </div>
+              <div className='TextField-container'>
+                <label htmlFor='Password-input'>Hasło</label>
+                <input type='password' id='Password-input' className='TextFields' placeholder='Wpisz hasło...'></input>
+              </div>
+              <div className='TextField-container'>
+                <label htmlFor='Password-input2'>Potwierdź hasło</label>
+                <input type='password' id='Password-input2' className='TextFields' placeholder='Powtórz hasło...'></input>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
       <footer className='App-footer'>
         <div id='Footer-top'>
